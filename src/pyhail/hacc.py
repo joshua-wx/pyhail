8dd27fc49abf9b680de66bab6e04897d77074ce5
Joshua Soderholm <joshua.soderholm@bom.gov.au>
2026-05-06 14:56:58 +1000

    review of hacc for bug and coding issues

--- src/pyhail/hacc.py
+++ src/pyhail/hacc.py
@@ -1,7 +1,7 @@
 """
 hAcc (Hail Accumulation) implementation
 
-Algorthim published by:
+Algorithm published by:
 Wallace, R., Friedrich, K., Kalina, E. A., & Schlatter, P. (2019). 
 Using Operational Radar to Identify Deep Hail Accumulations from Thunderstorms, 
 Weather and Forecasting, 34(1), 133-150. 
@@ -33,7 +33,7 @@ def pyart(
     sp_reflectivity_threshold=55,
 ):
     """
-    Pyart Wrapper for Hail Accumulation defined by Robinson et al. 2018 and Kalina et al. 2016.
+    Pyart Wrapper for Hail Accumulation defined by Wallace et al. 2019 and Kalina et al. 2016.
 
     Parameters:
     ===========
@@ -44,7 +44,7 @@ def pyart(
     sweep_idx : integer
         index of reflectivity sweep to use for hacc calculation
     mesh_idx : integer
-        integer of sweep continaing MESH data in the radar object
+        integer of sweep containing MESH data in the radar object
     fz_level: int
         wet bulb freezing level (m)
     pressure: float (1,)
@@ -66,7 +66,7 @@ def pyart(
 
     # init radar fields
     empty_radar_field = {
-        "data": np.zeros((radar.nrays, radar.ngates)),
+        "data": np.full((radar.nrays, radar.ngates), np.nan),
         "units": "",
         "long_name": "",
         "description": "",
@@ -86,7 +86,7 @@ def pyart(
     hacc_dict = main(
         radar.get_field(sweep_idx, reflectivity_fname, copy=True).filled(np.nan),
         hsda_field,
-        radar.get_field(mesh_idx, mesh_fname, copy=True),
+        radar.get_field(mesh_idx, mesh_fname, copy=True).filled(np.nan),
         sweep0_z + radar_altitude,
         fz_level,
         pressure,
@@ -113,7 +113,7 @@ def pyodim(
     sp_reflectivity_threshold=55,
 ):
     """
-    Pyodim Wrapper for Hail Accumulation defined by Robinson et al. 2018 and Kalina et al. 2016.
+    Pyodim Wrapper for Hail Accumulation defined by Wallace et al. 2019 and Kalina et al. 2016.
 
     Parameters:
     ===========
@@ -124,13 +124,11 @@ def pyodim(
     sweep_idx : integer
         index of reflectivity sweep to use for hacc calculation
     mesh_idx : integer
-        integer of sweep continaing MESH data in the radar object
+        integer of sweep containing MESH data in the radar object
     fz_level: int
         wet bulb freezing level (m)
     pressure: float (1,)
         mean pressure between the surface and the height of the 0C wet-bulb temperature
-    reflectivity_fname: string
-        name of reflectivity field
     hsda_fname: string
         name of hsda field
     mesh_fname: string
@@ -163,7 +161,7 @@ def pyodim(
     )
 
     # update data and metadata
-    datasets[sweep_idx] = datasets[sweep_idx].merge(
+    datasets[sweep_idx] = datasets[sweep_idx].assign(
         {hacc_fname: (("azimuth", "range"), hacc_dict["data"])}
     )
     # metadata
@@ -203,7 +201,8 @@ def main(
     sp_reflectivity_threshold: float
         value used to threshold reflectivity for single pol analysis
     Returns:
-        hAcc_meta: dict
+    ========
+    hAcc_meta: dict
         pyart field dictionary containing hAcc dataset
 
     """
@@ -223,15 +222,15 @@ def main(
     ze = 10.0 ** (reflectivity_sweep / 10.0)  # convert Z to Ze
     iwc_sweep = (
         (4.4 * 10**-5) * ze ** (0.71) / 1000
-    )  # Ice Water Content (kg m-3) derived from Ze follow Heysfield and Miller 1998
-    # remove IWC values where hail_hca is not hail (less than 1)
-    iwc_sweep[hail_hca < 1] = 0
+    )  # Ice Water Content (kg m-3) derived from Ze follow Heymsfield and Miller 1988
+    # remove IWC values where hail_hca is not hail
+    iwc_sweep[~hail_hca] = 0
     # remove IWC values where temperature is at or below 0
     iwc_sweep[z_sweep > fz_level] = 0
 
-    # pressure correction from Heysmfield and Write (2014)
+    # pressure correction from Heymsfield and Wright (2014)
     pc = (1000 / pressure) ** 0.545
-    # diameter-fall speed relation from Heysmfield and Wright (2014), units of cm/s
+    # diameter-fall speed relation from Heymsfield and Wright (2014), units of cm/s
     vt = 488 * (mesh_sweep / 10) ** 0.84 * pc
 
     # calculate LASH (units of cm/s)
